175b5b10b72d63e81280a58534f15f17e24b2d8f
DESKTOP-R1NT098\mrscr <cc.setwo@gmail.com>
2017-01-15 14:23:01 +0200

    no message

---
+++ Assets/GoToUrl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoToUrl : MonoBehaviour
+{
+
+    private string Url = "https://www.instagram.com/lucid_reality_labs/"
+
+;   // https://www.instagram.com/lucid_reality_labs/
+
+    private void OnMouseDown()
+    {
+        Application.OpenURL(Url);
+    }
+}
